f2119f304ff716466bb46e7280972fc2fa825667
arcolinuxz <arcolinuxinfo@gmail.com>
2024-02-15 11:18:48 +0100

    q and a

--- etc/skel/.config/nimdow/autostart.sh
+++ etc/skel/.config/nimdow/autostart.sh
@@ -3,6 +3,9 @@
 dex $HOME/.config/autostart/arcolinux-welcome-app.desktop &
 $HOME/.config/nimdow/nimdowstatus/NimdowStatus &
 
+# keyboard
+# setxkbmap be
+
 # xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 # xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 # xrandr --output eDP-1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
