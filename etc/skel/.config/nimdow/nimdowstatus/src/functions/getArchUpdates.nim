d2665e70b95b73e8bf50d37349692be69cbad0e8
arcolinuxz <arcolinuxinfo@gmail.com>
2024-05-19 10:05:40 +0200

    update

--- etc/skel/.config/nimdow/nimdowstatus/src/functions/getArchUpdates.nim
+++ etc/skel/.config/nimdow/nimdowstatus/src/functions/getArchUpdates.nim
@@ -2,21 +2,22 @@ var
   uTimeStamp: Option[DateTime]
   sUpdates: string = ""
 
-proc getArchUpdates(): string = 
-  if uTimeStamp.isSome and uTimeStamp.get() + initDuration(minutes = UPDATE_UPDATES) > now():
+proc getArchUpdates(): string =
+  if uTimeStamp.isSome and uTimeStamp.get() + initDuration(
+      minutes = UPDATE_UPDATES) > now():
     result = UPDATE_ICON & sUpdates
   else:
     uTimeStamp = some(now())
     try:
-      sUpdates = execProcess("checkupdates | wc -l") 
+      sUpdates = execProcess("checkupdates | wc -l")
+      discard parseInt(sUpdates)
       result = UPDATE_ICON & sUpdates
     except CatchableError as e:
-      echo "An Error: ", e.repr
       result = UPDATE_ICON & " N/A"
     finally:
-      discard 
+      discard
 
 
 
 
-  
+
